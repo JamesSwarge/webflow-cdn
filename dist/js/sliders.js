a26d47d8f40e53633df6bd0071f116cc359ffbb5
JamesSwarge <james.swarge@gmail.com>
2024-09-30 10:15:43 +0530

    * CSS JS changes 17.

--- dist/js/sliders.js
+++ dist/js/sliders.js
@@ -1,9 +1,34 @@
 
 $(document).ready(function () {
   // alert("slider.js");
+  $(".about-team-slider-2").slick({
+    centerMode: false,    
+    infinite: true,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    dots: false,
+    fade: false,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+    ],
+  });
+
   $(".about-team-slider").slick({
-    centerMode: false,
-    
+    centerMode: false,    
     infinite: true,
     autoplay: true,
     autoplaySpeed: 2000,
