1d4f23e3b6ee0b68ef4dd10e2b4d32721ab34188
JamesSwarge <james.swarge@gmail.com>
2024-10-07 14:36:45 +0530

    * CSS JS changes 47.

--- dist/js/sliders.js
+++ dist/js/sliders.js
@@ -137,10 +137,24 @@ if ($(window).width() > 992) {
     autoplay: true,
     autoplaySpeed: 3000,
     speed: 300,
-    slidesToShow: 1,
+    slidesToShow: 2,
     slidesToScroll: 1,
     dots: false,
-    fade: true
+    fade: true,
+    responsive: [
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 2,
+        },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+    ],
   });
 
 }
