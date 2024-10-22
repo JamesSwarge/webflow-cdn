b8fb494790899871711af2a6d5874274a432090c
JamesSwarge <james.swarge@gmail.com>
2024-10-22 09:47:33 +0530

    * CSS JS changes 76.

--- dist/js/sliders.js
+++ dist/js/sliders.js
@@ -128,6 +128,10 @@ $(document).ready(function () {
     pagination: {
       el: ".swiper-pagination",
     },
+    autoplay: {
+      delay: 3000,
+      disableOnInteraction: false,
+    },
   });
 
 
